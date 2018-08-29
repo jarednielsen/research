f1c32c3898c98453abfed737c3c3d09b43023fc8
Jared <jaredtnielsen@gmail.com>
2018-08-29 11:10:45 -0600

    first commit

---
+++ vae.py
@@ -0,0 +1,14 @@
+# vae.py
+
+"""
+Creates a variational autoencoder that works on MNIST images.
+See http://kvfrans.com/variational-autoencoders-explained/
+"""
+
+def run_vae():
+	pass
+
+
+
+if __name__ == "__main__":
+	run_vae()
